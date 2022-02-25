908c22ec43c80f463689ac0f50872739f0350fce
Alan Zhou <54216387+AlanReviews@users.noreply.github.com>
2022-02-24 19:00:26 -0500

    Added serverinfo command
    Added and updated serverinfo command

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -5,12 +5,13 @@ module.exports = {
 	name: 'serverinfo',
 	description: 'Gets server info',
 	data: new SlashCommandBuilder()
-		.setName('server')
+		.setName('serverinfo')
 		.setDescription('Gets server info in an embed'),
 	async execute(interaction) {
 		const serverInfo = new MessageEmbed()
         serverInfo.setTitle(interaction.guild.name)
 		serverInfo.addField("Members", `${interaction.guild.memberCount}`)
+		serverInfo.addField("NSFW Level", interaction.guild.nsfwLevel)
         await interaction.reply({embeds: [serverInfo], ephemeral: false });
 	},
 };
