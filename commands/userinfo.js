d251c250ec406ee69429b55747f390a421cd4b44
Alan Zhou <54216387+AlanReviews@users.noreply.github.com>
2022-03-05 20:37:48 -0500

    Added userinfo slash command
    Added the userinfo command.

---
+++ commands/userinfo.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	name: 'userinfo',
+	description: 'Gets user info',
+	data: new SlashCommandBuilder()
+		.setName('userinfo')
+		.setDescription('Gets user info in an embed')
+        .addUserOption(option =>
+            option
+			.setName('user')
+			.setDescription('The user you want to get their info with')
+			.setRequired(true)
+        ),
+	async execute(interaction) {
+        const member = interaction.options.getUser("user");
+		const userInfo = new MessageEmbed()
+        userInfo.setTitle(member.username)
+		userInfo.addField("ID", `${member.id}`)
+        await interaction.reply({embeds: [userInfo], ephemeral: false });
+	},
+};
